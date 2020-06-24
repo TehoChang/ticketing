da92ab2e88d0cc1fc7ea17f6b88628368d00dda9
Teho <changteho11@gmail.com>
2020-06-24 10:22:02 +0800

    add this fu*k ///

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://www.taipeitix.site',
+      baseURL: 'http://www.taipeitix.site/',
       headers: req.headers
     });
   } else {
