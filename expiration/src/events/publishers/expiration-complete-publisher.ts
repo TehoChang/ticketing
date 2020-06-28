a236d549d4acdf9b9bca8f1c594157f8055b99a8
Teho <changteho11@gmail.com>
2020-06-28 19:08:59 +0800

    update common module

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -2,7 +2,7 @@ import {
   Subjects,
   Publisher,
   ExpirationCompleteEvent,
-} from '@sgtickets/common';
+} from '@tehotickets/common';
 
 export class ExpirationCompletePublisher extends Publisher<
   ExpirationCompleteEvent
